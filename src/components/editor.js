84581a5a88a43de0205ce0fccf1d1ace895ddcd7
ghostminer <1696415745@qq.com>
2024-06-12 19:38:09 +0800

    Init

---
+++ src/components/editor.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export class MoonEditor extends React.Component {
+  modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6,false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+      ['link', 'image'],
+      ['clean']
+    ],
+  };
+
+  formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  handleChange(value) {
+    this.setState({ text: value });
+  }
+
+  render() {
+    return (
+      <ReactQuill
+        theme="snow"
+        value={this.state.text}
+        onChange={(value) => this.setState({ text: value })}
+        modules={this.modules}
+        formats={this.formats}
+      />
+    );
+  }
+}
